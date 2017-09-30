6f25f9dddcea4a3e2d17d91aecbc9c15c9c350b5
revsi <therevolutionizt@gmail.com>
2017-09-30 21:57:57 +0530

    Add support for multiple language

--- get_tweets.py
+++ get_tweets.py
@@ -10,16 +10,24 @@ def clean_tweet(tweet_string):
 	#[https://]*[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)
 	# capture ursl and emails
 
-data_file = open('./telugu_data.text', 'w')
+
+
+parser = argparse.ArgumentParser(description='This script scrap tweets')
+requiredNamed = parser.add_argument_group('required named arguments')
+requiredNamed.add_argument('-l', '--language', help='Language of tweets', required=True)
+
+
+
+data_file = open('./hindi_data.text', 'w')
 saved_tweets_id = {}
 saved_tweets_string = {}
 count = 0
-word_set = telugu_words
+word_set = hindi_words
 total_iterations = len(word_set)
 iterations = 0
 for query in word_set:
 	iterations += 1
-	for tweet in query_tweets(query, 200)[:200]:
+	for tweet in query_tweets(query, 500)[:500]:
 		tweet_text = clean_tweet(tweet.text)
 		if tweet.id not in saved_tweets_id and (len(tweet_text)>70 and tweet_text[:70] not in saved_tweets_string):
 			print(tweet_text, file=data_file)
