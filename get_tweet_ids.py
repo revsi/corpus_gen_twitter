1aec722b9ac9b1ef3c8b1c42ac23a2edfb4f2062
revsi <therevolutionizt@gmail.com>
2018-05-27 14:00:14 +0530

    update frequent words and retrieval of tweets

--- get_tweet_ids.py
+++ get_tweet_ids.py
@@ -25,24 +25,25 @@ from all_emojis import *
 
 
 query = ''
-# emoji_file = open('./hindi_data.label', 'w')
+emoji_file = open('./hindi_data.label', 'w')
 data_file = open('./hindi_data_new.ids', 'w')
+threshold = 150
 saved_tweets_id = {}
 count = 0
-word_set = hindi_words
+word_set = code_mixed_words
 total_iterations = len(word_set)*len(emojis)
 iterations = 0
 for emoji in emojis:
-	for query_words in word_set:
-		query = ' '.join([query_words, emoji])
+	for query_word in word_set:
+		query = ' '.join([query_word, emoji])
 		iterations += 1
-		for tweet in query_tweets(query, 150)[:150]:
+		for tweet in query_tweets(query, threshold)[:threshold]:
 			if tweet.id not in saved_tweets_id:
 				print(tweet.id, file=data_file)
-				# print(emojis[emoji], file=emoji_file)
+				print(emojis[emoji], file=emoji_file)
 				saved_tweets_id[tweet.id] = 1
 				count += 1
 		print('{} tweet ids saved. => {:.2f} % completed'.format(count, (iterations/total_iterations)*100.0))
-# emoji_file.close()
+emoji_file.close()
 data_file.close()
 			
